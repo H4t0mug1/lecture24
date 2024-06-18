a2d3db64b8276e3f333af494a7c3006c145e6190
H4t0mug1 <aichitead24410@gmail.com>
2024-06-18 13:59:41 +0900

    add bag

---
+++ src/kadai01.py
@@ -0,0 +1,6 @@
+import datetime
+
+x = datetime.date.today()
+y = ("Hello World!")
+
+print(f"{x},{y}")
---
+++ src/kadai02.py
@@ -0,0 +1,8 @@
+import random
+x = int(0)
+x = random.randint(0,10)
+
+if x>=5:
+	print("x is 5 or more")
+else:
+	print("x is less than 5")
